88edf9ae403e84690c63e3ed654b58a95332dad2
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-04-16 22:30:51 +0700

    Menambahakan menu utama database

--- src/com/nandaiqbalh/Main.java
+++ src/com/nandaiqbalh/Main.java
@@ -1,10 +1,29 @@
 package com.nandaiqbalh;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\t\t\t====================================");
         System.out.println("\t\t\t======= DATABASE KELOMPOK 28 =======");
         System.out.println("\t\t\t====================================");
+
+        // Membuat Scanner untuk mengambil input dari user
+        Scanner userInput = new Scanner(System.in);
+
+        String pilihanUser;
+        boolean isLanjutkan = true;
+
+        while (isLanjutkan) {
+            System.out.println("\nDATABASE KELOMPOK 28\n");
+            System.out.println("1.\tNama dan NIM");
+            System.out.println("2.\tAsal daerah");
+            System.out.println("3.\tHobi");
+            System.out.println("4.\tMedia sosial");
+
+            System.out.print("\nPilihan anda [1-4] : ");
+            pilihanUser = userInput.next();
+        }
     }
 }
